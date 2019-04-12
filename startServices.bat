9b84cb7c58f08ac959721a7e5cd78afbddb08b53
zhangcq <zhangcq@itcast.cn>
2019-04-12 16:46:18 +0800

    修改启动命令

--- startServices.bat
+++ startServices.bat
@@ -1,16 +1,14 @@
-@cd /d D:/Git/
-@cd clouddemo\spdemo
+@set curdir=%cd%
+@set fileDir=%~dp0
+@cd %fileDir%spdemo
 @start mvn spring-boot:run -Dport=8902  -Deureka.server=http://localhost:8801/eureka
 @start mvn spring-boot:run -Dport=8903  -Deureka.server=http://localhost:8801/eureka
 @start mvn spring-boot:run -Dport=8904  -Deureka.server=http://localhost:8801/eureka
 @start mvn spring-boot:run -Dport=8905  -Deureka.server=http://localhost:8801/eureka
 
-
-@cd /d D:/Git/
-@cd clouddemo\eurekademo
+@cd %fileDir%eurekademo
 @start mvn spring-boot:run -Dport=8801
 
-
-@cd /d D:/Git/
-@cd clouddemo\invoker
+@cd %fileDir%invoker
 @start mvn spring-boot:run -Dport=9901
+@cd %curdir%
